a023abb3786ec9c9aae4c98429f32643cac19973
ValerioMattera <matteravalerio@gmail.com>
2021-12-10 23:42:42 +0000

    solving no-useless-escape in regex

--- src/utils/parseInstagramPost.js
+++ src/utils/parseInstagramPost.js
@@ -2,7 +2,7 @@ import parse from 'html-react-parser';
 
 const parseInstagramPost = (text) => {
   const urlRegex =
-    /(([a-z]+:\/\/)?(([a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(:[0-9]{1,5})?(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&amp;]*)?)?(#[a-zA-Z0-9!$&'()*+.=-_~:@/?]*)?)(\s+|$)/gi;
+    /(([a-z]+:\/\/)?(([a-z0-9\\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(:[0-9]{1,5})?(\/[a-z0-9_\-\\.~]+)*(\/([a-z0-9_\-\\.]*)(\?[a-z0-9+_\-\\.%=&amp;]*)?)?(#[a-zA-Z0-9!$&'()*+.=-_~:@/?]*)?)(\s+|$)/gi;
 
   let parsedtext = text.replace(
     urlRegex,
